f1a7e042b97e496b6145e42499b265fdb421b9ae
LiamTreacy <c00298242@setu.ie>
2025-11-29 16:02:11 +0000

    new drawing for home page, done styling for both pages

--- svelte-app/src/routes/+page.svelte
+++ svelte-app/src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <h1>Welcome to my Ocean Zine</h1>
-
+<div class = "startingGrid">
+<img id="OC" src="/assets/oceanTitle.png" alt="Drawing of Red Crab">
+<p>This site is best suited to be read on a computer</p>
+</div>
 <a class="next-page" href="/About">Next Page</a>
 
 <style>
@@ -8,6 +11,35 @@
         font-family: 'Courier New', Courier, monospace;
         text-align: center;}
         
+.startingGrid{
+        display: grid;
+        grid-template-columns: 1fr; /*forces text to be under image */
+        gap: 20px;
+        background-color: #1c3abd;
+        padding: 30px;
+        border-radius: 25px;
+    }
+
+        #OC{ width: 600px;         
+    height: auto; /* image looks weird without auto */
+    margin: 0 auto;
+    background: rgba(255, 255, 255, 0.2);
+    border: 2px solid white;
+    padding: 20px;
+    border-radius: 10px;
+    display: block;
+}
+
+        p {
+        width: 100%;
+        font-family: "Courier New", monospace;
+        font-size: 20px;
+        font-weight: bold;
+        text-align: center;
+        align-self: center;
+        color: white;     
+        }
+
     .next-page {
         position: fixed;
         bottom: 20px;
--- svelte-app/src/routes/About/+page.svelte
+++ svelte-app/src/routes/About/+page.svelte
@@ -5,30 +5,42 @@
         console.log("About Page Loaded Up");
     });
 </script>
-
+<div class = "gridBlue">
 <h1> So what is this page about?</h1>
 <p> This web zine has been created for a college assignment, I have always had an interest in the Ocean
     and wanted to create a site about it, and some creatures and aspects of it which I find interesting.
 
     I hope you enjoy what you see!
 </p>
+</div>
 
 <a class="next-page" href="/OceanLife">Next Page</a>
 <a class ="previous_page" href ="/">Previous Page</a>
 
 <style>
-        h1 {color: #ffffff; 
+           h1 {color: #ffffff; 
         font-size: 25px;
         font-family: 'Courier New', Courier, monospace;
         text-align: center;}
 
         p { font-size: 18px;
-            font-weight: bold;
             font-family: 'Courier New', Courier, monospace;
             color: #ffffff; 
+            font-weight: bold;
             text-align: center;}
-
             
+            .gridBlue{
+        display: flex;;
+        flex-direction: column;
+        gap: 20px;
+        background-color: #1c3abd;
+        padding: 30px;
+        border-radius: 25px;
+        width: 75%;
+        align-items: center;
+        margin: 30px auto;
+/* code from conclusion */
+    }
    .next-page {
         position: fixed;
         bottom: 20px;
